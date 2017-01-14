368c28b0667914d7a8f21fe77a02ffe32a919240
groupers <phileas.hocquard@gmail.com>
2017-01-14 13:48:14 +0000

    Initial Extension

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
+// xhr.open("GET", chrome.extension.getURL(''), true);
+// xhr.send();
